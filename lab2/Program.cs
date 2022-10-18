1c88571ae15873167bdd177b73b939423c26fc1f
BartoszLukaszek <barti120@onet.pl>
2022-10-18 13:39:15 +0200

    Lab2

---
+++ lab2/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Application
+{
+    public static void Main(string[] args)
+    {
+
+    }
+
+    public static string Repeat(string s, int n)
+    {
+        string result = "";
+        for (int i = 0; i < n; i++)
+        {
+            result += s; //
+        }
+        return result;
+    }
+
+}
